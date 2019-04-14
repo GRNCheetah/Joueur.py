52fe8179c30d5385c7ed1c9bedd1efdbc010d308
TannerAMay <tamfrb@mst.edu>
2019-04-14 06:47:55 -0500

    refactor of spawning

--- games/stardash/Spawn.py
+++ games/stardash/Spawn.py
@@ -2,14 +2,12 @@ from math import cos
 from math import sin
 
 class Spawn:
-
-    runCnt = 0
-
     def __init__(self, p, g):
         self.player = p
         self.game = g
+        self.runCnt = 0
 
-    #Find the closest point to b that is on the circle of my movement range
+    #Find the closest point to b that is on the circle of my spawning range
     def findCX(self, aX, aY, bX, bY, r):
         return aX + r * ((bX - aX) / ((bX - aX) ** 2 + (bY - aY) ** 2) ** 0.5)
     def findCY(self, aX, aY, bX, bY, r):
@@ -40,6 +38,7 @@ class Spawn:
                 xClosestMiner = 9999999
                 yClosestMiner = 9999999
 
+                #find the closest miner to the transporter and do math to find the closest point on the edge of the circle
                 for u in self.player.units:
                     if u.job.title == "miner":
                         if u.x < xClosestMiner and u.y < yClosestMiner:
@@ -75,6 +74,7 @@ class Spawn:
             xClosestTrans = 9999999
             yClosestTrans = 9999999
 
+            #Spawn the martyr as close as possible to the closest transporter
             for u in self.player.units:
                 if u.job.title == "transport":
                     if u.x < xClosestTrans and u.y < yClosestTrans:
@@ -83,17 +83,20 @@ class Spawn:
 
             print("martyrs:", xClosestTrans, xClosestTrans)
 
+            #spawn two
             if not self.player.home_base.spawn(xClosestTrans, yClosestTrans, "martyr"):
                 self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "martyr")
             if not self.player.home_base.spawn(xClosestTrans, yClosestTrans, "martyr"):
                 self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "martyr")
-        elif self.runCnt > 2:
+        elif self.runCnt > 2: #alternate spawning miners and corvettes
             if self.runCnt % 2 == 1: #miners
-                while self.player.money >= 150:
+                breakout = 1 # breaks out of while after a couple iterations of not being decremented
+                while self.player.money >= 150 and breakout < 5:
                     if amtMiners % 5 == 0: #make a transport every 5 miners
                         xClosestMiner = 9999999
                         yClosestMiner = 9999999
 
+                        # find the closest miner to the transporter and do math to find the closest point on the edge of the circle
                         for u in self.player.units:
                             if u.job.title == "miner":
                                 if u.x < xClosestMiner and u.y < yClosestMiner:
@@ -105,7 +108,10 @@ class Spawn:
                         print("trans:", xClosestMiner, yClosestMiner)
 
                         if not self.player.home_base.spawn(xClosestMiner, yClosestMiner, "transport"):
-                            self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "transport")
+                            if not self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "transport"):
+                                breakout += 1
+
+                        continue
 
                     xClosestResource = 9999999
                     yClosestResource = 9999999
@@ -123,12 +129,14 @@ class Spawn:
                     print("miners:", xClosestResource, yClosestResource)
 
                     if not self.player.home_base.spawn(xClosestResource, yClosestResource, "miner"):
-                        self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "miner")
+                        if not self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "miner"):
+                            breakout += 1
+                            continue
 
                     amtMiners += 1
-            else:
+            else: #corvettes
                 while self.player.money >= 100:
-                    if amtCorv % 2 == 0: #spawn corvett up at 45 degrees
+                    if amtCorv % 2 == 0: #spawn corvette up at 45 degrees
                         xCoord = self.player.home_base.x + self.player.home_base.radius * cos(45)
                         yCoord = self.player.home_base.y + self.player.home_base.radius * sin(45)
 
@@ -136,7 +144,7 @@ class Spawn:
 
                         if not self.player.home_base.spawn(xCoord, yCoord, "corvette"):
                             self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "corvette")
-                    else:
+                    elif amtCorv % 2 == 1: #spawn corvette down at 45 degrees (315 degrees)
                         xCoord = self.player.home_base.x + self.player.home_base.radius * cos(315)
                         yCoord = self.player.home_base.y + self.player.home_base.radius * sin(315)
 
@@ -144,6 +152,8 @@ class Spawn:
 
                         if not self.player.home_base.spawn(xCoord, yCoord, "corvette"):
                             self.player.home_base.spawn(self.player.home_base.x, self.player.home_base.y, "corvette")
+                    else:
+                        break
 
                     amtCorv += 1
 
